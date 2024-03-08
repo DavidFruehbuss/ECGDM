e0631e97edc33039d1747b0d3bd0ac02cd13ad9b
David Frühbuß <david.fruehbuss@outlook.de>
2024-03-08 12:24:12 +0100

    (D) forgot to call super().__init__()

--- Models/architecture.py
+++ Models/architecture.py
@@ -23,6 +23,7 @@ class NN_Model(nn.Module):
             num_residues: int,
             device: str,
     ):
+        super().__init__()
         self.architecture = architecture
         self.x_dim = 3
         self.act_fn = nn.SiLU()
