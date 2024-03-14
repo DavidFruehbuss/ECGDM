c9ed91376de647ee3d2908e654c7f83c4112e319
David Frühbuß <david.fruehbuss@outlook.de>
2024-03-14 18:20:11 +0100

    (D) tracking down nan values in loss_0

--- Data/Peptide_data/dataset_pmhc.py
+++ Data/Peptide_data/dataset_pmhc.py
@@ -4,3 +4,8 @@ load data into correct format:
 2. graphs with additional energy labels
 '''
 
+import h5py
+
+with h5py.File('/gpfs/work3/0/einf2380/data/pMHCI/features_output_folder/GNN/residue/230201/residue-4162789.hdf5', 'r') as f5:
+
+    print(f5.keys())
