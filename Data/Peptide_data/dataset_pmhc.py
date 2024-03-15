aaeb757ce1fceb48b731fed5abee915dfcc33c20
David Frühbuß <david.fruehbuss@outlook.de>
2024-03-15 20:58:47 +0100

    (D) ...

--- Data/Peptide_data/dataset_pmhc.py
+++ Data/Peptide_data/dataset_pmhc.py
@@ -9,3 +9,18 @@ import h5py
 with h5py.File('/gpfs/work3/0/einf2380/data/pMHCI/features_output_folder/GNN/residue/230201/residue-4162789.hdf5', 'r') as f5:
 
     print(f5.keys())
+
+
+datadir = '/gpfs/work3/0/einf2380/data/pMHCI/features_output_folder/GNN/residue/230201/'
+
+data = []
+
+for file_name in datadir:
+
+    with h5py.File(f'{datadir}/{file_name}', 'r') as f5:
+
+        data_subset = f5
+
+    for graph_names, graph in f5.itmes():
+
+        data.append(graph)
