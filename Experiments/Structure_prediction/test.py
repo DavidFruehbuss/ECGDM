f903e0bfaa3d2f7753799a25cbc5dd019542b519
David Frühbuß <david.fruehbuss@outlook.de>
2024-04-09 19:38:37 +0200

    (D) ...

--- Experiments/Structure_prediction/test.py
+++ Experiments/Structure_prediction/test.py
@@ -11,63 +11,64 @@ from torch_scatter import scatter_add
 from ECGDM.Experiments.Structure_prediction.lightning_module import Structure_Prediction_Model
 from Data.Peptide_data.dataset_pmhc import Peptide_MHC_Dataset
 
+if __name__ == "__main__":
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-# read in config
-parser = argparse.ArgumentParser()
-parser.add_argument('--config', type=str, required=True)
-args = parser.parse_args()
+    # read in config
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, required=True)
+    args = parser.parse_args()
 
-with open(args.config) as f:
-    config = yaml.safe_load(f)
+    with open(args.config) as f:
+        config = yaml.safe_load(f)
 
-args_dict = args.__dict__
-for key, value in config.items():
-    if isinstance(value, dict):
-        args_dict[key] = Namespace(**value)
-    else:
-        args_dict[key] = value
+    args_dict = args.__dict__
+    for key, value in config.items():
+        if isinstance(value, dict):
+            args_dict[key] = Namespace(**value)
+        else:
+            args_dict[key] = value
 
-num_samples = args.num_samples
+    num_samples = args.num_samples
 
-lightning_model = Structure_Prediction_Model.load_from_checkpoint(
-                args.checkpoint,
-                dataset=args.dataset,
-                data_dir=args.data_dir,
-                dataset_params=args.dataset_params,
-                task_params=args.task_params,
-                generative_model=args.generative_model,
-                generative_model_params=args.generative_model_params,
-                architecture=args.architecture,
-                network_params=args.network_params,
-                batch_size=args.batch_size,
-                lr=args.lr,
-                num_workers=args.num_workers,
-                device=args.device,
-)   
+    lightning_model = Structure_Prediction_Model.load_from_checkpoint(
+                    args.checkpoint,
+                    dataset=args.dataset,
+                    data_dir=args.data_dir,
+                    dataset_params=args.dataset_params,
+                    task_params=args.task_params,
+                    generative_model=args.generative_model,
+                    generative_model_params=args.generative_model_params,
+                    architecture=args.architecture,
+                    network_params=args.network_params,
+                    batch_size=args.batch_size,
+                    lr=args.lr,
+                    num_workers=args.num_workers,
+                    device=args.device,
+    )   
 
-lightning_model = lightning_model.to(device)
-lightning_model.setup('test')
-test_dataset = lightning_model.test_dataset
+    lightning_model = lightning_model.to(device)
+    lightning_model.setup('test')
+    test_dataset = lightning_model.test_dataset
 
-results = []
+    results = []
 
-for i, mol_pro in enumerate(test_dataset):
+    for i, mol_pro in enumerate(test_dataset):
 
-    # prepare peptide-MHC
-    molecule, protein_pocket = mol_pro
-    mol_pro_list = [(mol_pro) for i in range(num_samples)]
-    mol_pro_samples = Peptide_MHC_Dataset.collate_fn(mol_pro_list)
+        # prepare peptide-MHC
+        molecule, protein_pocket = mol_pro
+        mol_pro_list = [(mol_pro) for i in range(num_samples)]
+        mol_pro_samples = Peptide_MHC_Dataset.collate_fn(mol_pro_list)
 
-    # sample new peptide-MHC structures using trained model
-    mol_pro_batch = lightning_model.get_molecule_and_protein(mol_pro_samples)
-    xh_mol_final, xh_pro_final = lightning_model.model.sample(mol_pro_batch)
+        # sample new peptide-MHC structures using trained model
+        mol_pro_batch = lightning_model.get_molecule_and_protein(mol_pro_samples)
+        xh_mol_final, xh_pro_final = lightning_model.model.sample(mol_pro_batch)
 
-    # Calculate the RMSE error
-    rmse = scatter_add(torch.sqrt(torch.sum((molecule['x'] - xh_mol_final[:,:3])**2, dim=-1)), molecule['idx'], dim=0)
+        # Calculate the RMSE error
+        rmse = scatter_add(torch.sqrt(torch.sum((molecule['x'] - xh_mol_final[:,:3])**2, dim=-1)), molecule['idx'], dim=0)
 
-    results += [mol_pro, (xh_mol_final, xh_pro_final), rmse]
+        results += [mol_pro, (xh_mol_final, xh_pro_final), rmse]
 
-print(results)
+    print(results)
 
