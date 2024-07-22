changed rsme computation to correct value

Experiments/Structure_prediction/test.py
@@ -121,7 +121,7 @@ if __name__ == "__main__":
 
         # Calculate the RMSE error
         error_mol = scatter_add(torch.sum((molecule['x'] - xh_mol_final[:,:3])**2, dim=-1), molecule['idx'], dim=0)
-        rmse = torch.sqrt(error_mol / (3 * molecule['size']))
+        rmse = torch.sqrt(error_mol / (molecule['size']))
 
         rmse_sample_mean = [rmse[j*num_samples:(j+1)*num_samples].mean(0) for j in range(sample_batch_size)]
         rmse_sample_best = [rmse[j*num_samples:(j+1)*num_samples].min(0)[0] for j in range(sample_batch_size)]

Models/architecture.py
@@ -80,7 +80,7 @@ class NN_Model(nn.Module):
 
             # dimensions for ponita model
             in_channels_scalar = self.joint_dim
-            in_channels_vec = 0 # shouldn't this be 1 or does it take the pos by default?
+            in_channels_vec = 1 # shouldn't this be 1 or does it take the pos by default?
             # TODO: check how to properly use scalar vs vector outputs
             out_channels_scalar = self.joint_dim # updated features
             out_channels_vec = 1 # displacment vector